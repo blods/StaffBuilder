538a6d462fcb5db009d00b031c02e78e6f8c7e26
blods <robin_sayer@hotmail.com>
2014-11-01 00:29:46 -0700

    Basic timer job

---
+++ StaffBuider/StaffBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint; 
+
+namespace StaffBuider
+{
+    public class StaffBuilder:SPJobDefinition
+    {
+        public StaffBuilder():base() { }
+
+        public StaffBuilder(SPWebApplication webApplication)
+            : base(
+            "StaffBuilder", webApplication, null, SPJobLockType.Job)
+        {
+            this.Title = "Staff Builder Job";
+        }
+
+        public override void Execute(Guid targetInstanceId)
+        {
+            base.Execute(targetInstanceId);
+
+            // Execute Your Code Here
+        }
+    }
+}
